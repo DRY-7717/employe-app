b8337c6dde254ee66cf0ce97ce0ac4fe2d7d6cf1
DRY-7717 <wicaksanabimaarya01@gmail.com>
2024-06-24 14:08:19 +0700

    redesign login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-5 col-12 mt-5">
-                <div class="card  shadow-sm  " style="border-top: 3.5px solid #0d6efd;">
-                    <div class="card-body">
-                        <h3 class="fw-bold ">Login</h3>
-                        <p class="p-0 mt-0 mx-0 mb-2 text-secondary" style="font-size: 16px">Selamat datang di aplikasi
-                            Employee App. Silahkan login!</p>
-                        @if (session('message'))
-                            <div class="alert alert-danger" role="alert">
-                                {{ session('message') }}
+    <section class="w-100"
+        style="background: url('../img/4.jpeg'); background-size: cover; background-position: center; height: 100vh;">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-md-5 col-12 " style="margin-top: 85px; margin-right: -50px" >
+                    <div class="card  shadow-sm">
+                        <div class="card-body">
+                            <div class="box-logo mb-4" style="width: 230px;">
+                                <img src="/img/logo.png" style="object-fit: cover; width: 100%;" alt="">
                             </div>
-                        @endif
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-12 col-form-label ">{{ __('Email Address') }}</label>
-
-                                <div class="col-md-12">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                            <h3 class="fw-bold ">Login</h3>
+                            <p class="p-0 mt-0 mx-0 mb-2 text-secondary" style="font-size: 16px">Selamat datang di aplikasi
+                                Employee App. Silahkan login!</p>
+                            @if (session('message'))
+                                <div class="alert alert-danger" role="alert">
+                                    {{ session('message') }}
                                 </div>
-                            </div>
+                            @endif
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="row mb-3">
+                                    <label for="email"
+                                        class="col-md-12 col-form-label ">{{ __('Email Address') }}</label>
 
-                            <div class="row mb-3">
-                                <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
+                                    <div class="col-md-12">
+                                        <input id="email" type="email"
+                                            class="form-control @error('email') is-invalid @enderror" name="email"
+                                            value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                <div class="col-md-12">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mb-0">
-                                <div class="col-md-12 text-cetner">
-                                    <button type="submit" class="w-100 d-block btn btn-primary mb-3">
-                                        {{ __('Login') }}
-                                    </button>
-                                    {{-- <p class="text-center text-secondary m-0 p-0"> Sudah Punya akun? <a href="/register" class=" text-center text-decoration-none">register</a></p> --}}
 
+                                <div class="row mb-3">
+                                    <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
 
-                                    {{-- @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif --}}
+                                    <div class="col-md-12">
+                                        <input id="password" type="password"
+                                            class="form-control @error('password') is-invalid @enderror" name="password"
+                                            required autocomplete="current-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                                <div class="row mb-0">
+                                    <div class="col-md-12 text-cetner">
+                                        <button type="submit" class="w-100 d-block btn btn-primary mb-3">
+                                            {{ __('Login') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body class="">
     <div id="app">
         {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -72,7 +72,7 @@
             </div>
         </nav> --}}
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
